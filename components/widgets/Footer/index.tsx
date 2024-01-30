3689816b675983cff6c3626a42f037af87a01ad4
syedsaif666 <saifuddin666@protonmail.com>
2024-01-30 19:15:36 +0500

    fix: footer link 2

--- components/widgets/Footer/index.tsx
+++ components/widgets/Footer/index.tsx
@@ -51,12 +51,12 @@ const Footer = ({data} : {data: TDictionary}) => {
         </p>
         <div className="flex gap-6 justify-center items-center">
           {data.footerSection.links.map((item) => (
-            <Link key={item.name} href="/">
+            <Link key={item.name} href="/"
                className={classNames(
                 item.name ? 'text-[#9CA3AF]' : 'text-slate-300 py-2',
                 'text-xs lg:text-sm font-medium tracking-[0.00438rem]'
               )}
-              aria-current={item.name ? 'page' : undefined}
+              aria-current={item.name ? 'page' : undefined}>
                 {item.name}
               {/* </p> */}
             </Link>
